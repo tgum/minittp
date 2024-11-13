8ffa874f94ac7eb59cf8cff2efb14ff98d2717f4
tgum <3004505+tgum@users.noreply.github.com>
2024-11-13 17:36:29 +0100

    Removed old server
    Added formatting and fixed the example

--- server.py
+++ server.py
@@ -1,102 +1,129 @@
-import socket
-import sys
-import os
-import mimetypes
-from pprint import pprint
+# from pprint import pprint
 import re
+import asyncio
+import traceback
 
 import chevron
 
 from request import Request
 from response import Response
 
-# create an INET, STREAMing socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# so it like yk can reuse the same port or smth
-serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# bind the socket to a public host, and a well-known port
-serversocket.bind(("localhost", 8080))
-# become a server socket
-serversocket.listen(5)
-
 NEWLINE = b"\r\n"
 
-paths = []
-def register_handler(path, handler):
-	paths.append({"regex": re.compile(path), "handler": handler})
 
-def static_serve(req):
-	res = Response()
+# this is an interface to make async sockets work more like
+# normal ones or smth i forgot
+class RequestWrapper:
+    def __init__(self, reader, writer):
+        self.reader = reader
+        self.writer = writer
 
-	path = "www" + req.path
-	if path.endswith("/"):
-		path += "index.html"
+    async def recv(self, amount=1024):
+        return await self.reader.read(amount)
 
-	mime, _ = mimetypes.guess_type(path)
-	res.headers["Content-Type"] = f"{mime or 'text/html'}; charset=UTF-8"
+    def write(self, data):
+        self.writer.write(data)
 
-	if os.path.isfile(path):
-		with open(path, "rb") as f:
-			res.body = f.read()
-	else:
-		with open("www/404.html", "rb") as f:
-			res.body = f.read()
-			res.status = 404
-			res.headers["Content-Type"] = f"{mime}; charset=UTF-8"
+    async def drain(self):
+        await self.writer.drain()
 
-	return res
+    async def send(self, data):
+        self.write(data)
+        await self.drain()
 
-count = 0
-def get_counter():
-	global count
-	count += 1
-	return count
+    def close(self):
+        self.writer.close()
 
-def counter_page(req):
-	res = Response()
-	res.body = str(get_counter())
-	return res
 
-def template_serve(req):
-	res = Response()
-	with open("www/template.html") as f:
-		template = f.read()
-	res.body = chevron.render(template, {"title": "hi"})
-	return res
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
 
-register_handler(r"^/.+\.html", static_serve)
-register_handler(r"^/frog", static_serve)
-register_handler(r"^/.*", static_serve)
-register_handler(r"^/counter", counter_page)
-register_handler(r"^/template", template_serve)
+        self.routes = []
 
-while True:
-	print("get connection")
-	# accept connections from outside
-	(clientsocket, address) = serversocket.accept()
-	# now do something with the clientsocket
-	
-	print("CONNECTION!!!")
+    def register_handler(self, path, handler, full_match=True):
+        print("register handler")
+        if full_match:
+            path = f"^{path}$"
+        self.routes.append(
+            {"type": "handler", "regex": re.compile(path), "handler": handler}
+        )
 
-	req = Request()
-	req.read_socket(clientsocket)
+    def internal_redirect(self, path, replacer, full_match=True):
+        print("register redirect")
+        if full_match:
+            path = f"^{path}$"
+        self.routes.append(
+            {"type": "redirect", "regex": re.compile(path), "replacer": replacer}
+        )
 
-	pprint(req.method)
-	pprint(req.path)
-	pprint(req.version)
-	pprint(req.headers)
+    def start(self):
+        asyncio.run(self._start())
 
-	response = None
-	for path in reversed(paths):
-		if path["regex"].match(req.path):
-			print(f"{req.path} matches {path['regex']}")
-			response = path["handler"](req)
-			if isinstance(response, Response):
-				break
-		else:
-			print(f"{req.path} does not match {path['regex']}")
+    async def _start(self):
+        server = await asyncio.start_server(self._handle_conn, self.host, self.port)
+        print(f"Serving on {self.host}:{self.port}!")
 
-	print(response)
-	response.headers["Server"] = "bascket"
-	clientsocket.send(response.get_res_text())
-	clientsocket.close()
+        await self.on_start()
+
+        async with server:
+            await server.serve_forever()
+
+    async def on_start(self):
+        print("server started!")
+
+    def handler_404(self, req):
+        res = Response()
+        with open("templates/404.mustache", "r") as f:
+            body = f.read()
+            res.body = chevron.render(body, {"url": req.path})
+            res.status = 404
+            res.headers["Content-Type"] = "text/html; charset=UTF-8"
+        return res
+
+    def handler_500(self, req, error=""):
+        res = Response()
+        with open("templates/500.mustache", "r") as f:
+            body = f.read()
+            res.body = chevron.render(body, {"error": error})
+            res.status = 500
+            res.headers["Content-Type"] = "text/html; charset=UTF-8"
+        return res
+
+    async def _handle_conn(self, reader, writer):
+        socket = RequestWrapper(reader, writer)
+        print("connection!!!")
+
+        req = Request()
+        await req.read_socket(socket)
+
+        print(f"{req.method} {req.path} {req.version}")
+
+        response = None
+        for path in reversed(self.routes):
+            if path["type"] == "handler":
+                if path["regex"].match(req.path):
+                    try:
+                        response = path["handler"].handler(req)
+                    except Exception:
+                        error_text = traceback.format_exc()
+                        print("\033[41m", end="")
+                        print(error_text)
+                        print("\033[49m")
+                        response = self.handler_500(req, error_text)
+                    if isinstance(response, Response):
+                        break
+            elif path["type"] == "redirect":
+                if path["regex"].match(req.path):
+                    replaced = path["regex"].sub(path["replacer"], req.path)
+                    print(f"-> {req.method} {replaced}")
+                    req.path = replaced
+        if not isinstance(response, Response):
+            response = self.handler_404(req)
+
+        response.headers["Server"] = "bascket"
+        await socket.send(response.get_res_text())
+        socket.close()
+        print("closed")
+        print()
